f7bdb3b6ec6e349aa44dba2a0e6368dd1ef25a8e
Ashy-codes-git <aiswaryavvv123@gmail.com>
2025-03-01 15:45:13 +0530

    Added careers page

---
+++ src/lib/constants.ts
@@ -0,0 +1,21 @@
+export const jobs = [
+    {
+        title: "React Developer",
+        logo: "/logos/react.png",
+        alt: "react logo",
+        category: "Web Development",
+    },
+    {
+        title: "Flutter Developer",
+        logo: "/logos/flutter.png",
+        alt: "flutter logo",
+        category: "App Development",
+    },
+    {
+        title: "Figma Designer",
+        logo: "/logos/figma.png",
+        alt: "figma logo",
+        category: "UI/UX",
+    },
+
+]
